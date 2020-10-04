0f938c9f27672530334866ead49779a5356b2c82
Lucas H <61823872+jhsi517@users.noreply.github.com>
2020-10-04 09:09:04 +0800

    Extract reduced feature files
    Reduced feature file contain features from 4 classes only: i4, pl40, pl80 and w57. They all have ~500 images per class.

--- traffic-sign-classification/1-svm/extract.py
+++ traffic-sign-classification/1-svm/extract.py
@@ -4,12 +4,14 @@ from feature_extractor import extract_features_and_save
 
 
 def main(args):
+    reducedDir = ['i4', 'pl30', 'pl80', 'w57']
     if args.train_dir:
         train_dirs = [os.path.join(args.train_dir, f) for f in os.listdir(args.train_dir)]
         train_files = []
         for subDir in train_dirs:
-            for file in os.listdir(subDir):
-                train_files.append(os.path.join(subDir, file))
+            if args.reduced and subDir.split('\\')[-1] in reducedDir:
+                for file in os.listdir(subDir):
+                    train_files.append(os.path.join(subDir, file))
         extract_features_and_save(train_files, os.path.join(args.output_dir, 'train'))
 
     if args.test_dir:
@@ -19,10 +21,18 @@ def main(args):
 
 
 if __name__ == '__main__':
+    outputDir = os.path.split(os.getcwd())[0] + r"\1-svm\Features\Reduced Features"
+    trainDataDir = os.path.dirname(os.path.split(os.getcwd())[0]) + r"\Data\Train"
+    testDataDir = os.path.dirname(os.path.split(os.getcwd())[0]) + r"\Data\Test"
+
     parser = ArgumentParser()
     parser.add_argument('--train_dir', type=str)
     parser.add_argument('--test_dir', type=str)
     parser.add_argument('--output_dir', type=str, default='./features/')
-    args = parser.parse_args()
-
+    parser.add_argument('--reduced', action='store_false')
+    # args = parser.parse_args()
+    args = parser.parse_args(['--train_dir', trainDataDir,
+                              '--test_dir', testDataDir,
+                              '--output_dir', outputDir])
+                              # '--reduced', True])
     main(args)
