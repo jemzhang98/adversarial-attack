9668edc292fb758beee2392baa4c5cc4ed6e5595
Lucas H <61823872+jhsi517@users.noreply.github.com>
2020-10-05 15:38:01 +0800

    Create square attack and store adversairal examples from FGSM

--- traffic-sign-classification/3-attack/Generated Adversarial Data/show_adversarial_image.py
+++ traffic-sign-classification/3-attack/Generated Adversarial Data/show_adversarial_image.py
@@ -2,14 +2,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-filePath = r'.\square_svm_color_adv.npy'
+filePath = r'.\square_svm_adv.npy'
 advData = np.load(filePath)
-if advData.dtype != np.unit8:
-    advData = advData.astype(np.unit8)
+if advData.dtype != np.uint8 and advData.shape[3] != 1:
+    advData = advData.astype(np.uint8)
 
 for image in advData:
     # showImage = np.reshape(image, (50, 50))
     # plt.imshow(showImage)
-    plt.imshow(image)
+    if image.shape[2] == 1:
+        plt.imshow(image, cmap='gray')
+    else:
+        plt.imshow(image)
     plt.show()
 
--- traffic-sign-classification/3-attack/fgsm_svm.py
+++ traffic-sign-classification/3-attack/fgsm_svm.py
@@ -7,7 +7,7 @@ import os
 import cv2
 import tensorflow as tf
 
-from art.attacks.evasion import DeepFool, UniversalPerturbation
+from art.attacks.evasion import DeepFool, UniversalPerturbation, SquareAttack
 from art.estimators.classification import KerasClassifier, SklearnClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
@@ -53,7 +53,7 @@ def convertLabel(labels):
 # Set training dataset directory and limiting the numbers to 2 category
 DATADIR = os.path.dirname(os.path.split(os.getcwd())[0]) + r"\Data\Train"
 CATEGORIES = ['i4', 'pl30', 'pl80', 'w57']
-IMG_SIZE = 50
+IMG_SIZE = 40
 
 tf.compat.v1.disable_eager_execution()
 
@@ -101,22 +101,26 @@ classifier = SklearnClassifier(model=model, clip_values=(0, 1))
 x_train_n = transform2Grey(x_train)
 x_test_n = transform2Grey(x_test)
 
+# save to npy file
+print('Saving testing data')
+save(r'./Generated Adversarial Data/svm_testing_data.npy', x_test_n)
+
 y_train_n = convertLabel(y_train)
 y_test_n = convertLabel(y_test)
 # Step 4: Train the ART classifier
 classifier.fit(x_train_n, y_train_n)
 predictions = classifier.predict(x_test_n)
-accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test_n, axis=1)) / len(y_test)
-
+accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test_n, axis=1)) / len(y_test_n)
 print("Accuracy on benign test examples: {}%".format(accuracy * 100))
+
+
 # Step 6: Generate adversarial test examples
-# attack = FastGradientMethod(estimator=classi  ssssfier, eps=0.2)
-attack = UniversalPerturbation(classifier=classifier, attacker='fgsm')
-x_test_adv = attack.generate(x=x_test_n)
+attack = UniversalPerturbation(classifier=classifier, attacker='fgsm', norm=2)
+x_test_adv = attack.generate(x=x_test)
 
 # save to npy file
 print('Saving generated adv data')
-save(r'./Generated Adversarial Data/uni_fgsm_svm_adv.npy', x_test_adv)
+save(r'./Generated Adversarial Data/square_svm_adv.npy', x_test_adv)
 
 # Step 7: Evaluate the ART classifier on adversarial test examples
 x_test_adv_flat = transform2Grey(x_test_adv)
